ba46b6d14d31b9494c53b6236ad6393cc3458761
brawlingNavada <JCLacivita@gmail.com>
2017-02-10 17:42:01 -0700

    fixed merge

--- public/app/components/collection/e-service.js
+++ public/app/components/collection/e-service.js
@@ -4,30 +4,35 @@ function LandingService(){
     var allPosts = [
         {
             id: 123,
-            author: "timmy",
-            title: "whatever",
+            user: "Jamie Lacivita",
+            title: "How to buy a cat",
             comments: [{}],
             date: "1/2/13",
             vote: 22,
-            content: "My QUestion? "
+            content: "How do I buy a cat?",
+            type: "question"
+            
         },
         {
             id: 124,
-            author: "timmy",
-            title: "whatever image",
+            user: "Rachel Mason",
+            title: "whatever link",
             comments: [{}],
             date: "1/2/13",
             vote: 2,
-            content: "placehold.it/200x200"
+            content: "http://www.google.com",
+            type: "link"
         },
         {
             id: 125,
-            author: "timmy",
-            title: "whatever",
+            user: "Arg the pirate",
+            title: "this is the title of the image",
             comments: [{}],
             date: "1/2/13",
             vote: 22,
-            content: "google.com"
+            content: "http://boattoursjohnspass.com/pirate-ship-cruise/wp-content/uploads/2013/05/home-slide-photo1.jpg",
+            type: "image"
+            
         }
     ]
 
