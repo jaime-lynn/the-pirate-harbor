df7fc139347599f87aa6535d6ee910a1e602ed3f
rachelmason <ramason@icloud.com>
2017-02-10 18:08:44 -0700

    form fix

--- public/app/components/form/form-service.js
+++ public/app/components/form/form-service.js
@@ -1,14 +1,15 @@
 function FormService(){
     var form = {}
-this.submit = function(title, content, type, author){
+this.submit = function(title, content, type, user){
 let newSubmit = {
     title:  title,
     content: content,
     type: type,
-    author: author,
+    user: user,
     date: Date.now()
 }
 form = newSubmit
+console.log(form)
 //this is where we will do a post request later
 } 
 
