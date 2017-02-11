e601fcd5c8db78e224e7f4daf73d7477fad99524
rachelmason <ramason@icloud.com>
2017-02-10 20:39:47 -0700

    login

--- public/app/main-service.js
+++ public/app/main-service.js
@@ -1,26 +1,26 @@
-function MainService(){
+function MainService() {
 
-this.login = function(username, password){
-var userLogin = {
-    username: username,
-    password: password
-}
-$.post("./login", userLogin ,function(user){
-    alert(username + " has logged in")
-    return {username:user.username, _id:user._id}
-})
-}
+    this.login = function (username, password) {
+        var userLogin = {
+            username: username,
+            password: password
+        }
+        $.post("./login", userLogin, function (user) {
+            alert(username + " has logged in")
+            return { username: user.username, _id: user._id }
+        })
+    }
 
-this.register = function(username, password){
-var userLogin = {
-    username: username,
-    password: password
-}
-$.post("./register", userLogin ,function(user){
-    alert(username + " has been created")
-    return {username:user.username, _id:user._id}
-})
-}
+    this.register = function (username, password) {
+        var userLogin = {
+            username: username,
+            password: password
+        }
+        $.post("./register", userLogin, function (user) {
+            alert(username + " has been created")
+            return { username: user.username, _id: user._id }
+        })
+    }
 
 
 
