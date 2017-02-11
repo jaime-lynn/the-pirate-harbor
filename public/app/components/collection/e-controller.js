128fa6c83234a736979fc47cc48e7e09ef6cc038
rachelmason <ramason@icloud.com>
2017-02-11 02:03:08 -0700

    show/hide

--- public/app/components/collection/e-controller.js
+++ public/app/components/collection/e-controller.js
@@ -9,24 +9,30 @@ Vue.component('mainpage', {
     mounted: function () {
         this.getData()
     },
+
     methods: {
         getData: function () {
             es.getAllPosts(this.setData)
         },
         setData: function (result) {
             this.allPosts = result.data
+        },
+        openPost:function(postId){
+            this.$root.$data.postId = postId
+            this.$root.$data.postPage = true
+            this.$root.$data.mainPage = false
         }
     },
     //if you can read this you are here.
     template: `
-    <div>
+    <div v-if="this.$root.$data.mainPage">
 
     
 
        <div v-for="post in allPosts">
         {{post._id}}
         <div v-if="post.type == 'question'">
-            <h1>{{post.title}} - {{post.username}}</h1> 
+            <h1><span @click="openPost(post._id)">{{post.title}} - {{post.username}}</span></h1> 
             <p>{{post.content}}</p> 
             <p>Vote: {{post.vote}}</p>
             <button v-on:click="tryVote(up)"> Vote up</button>
--- public/app/components/postpage/p-controller.js
+++ public/app/components/postpage/p-controller.js
@@ -3,43 +3,16 @@ Vue.component('postpage',{
     data: function(){
         return {
             post: {},
-<<<<<<< HEAD
-            postTitle: '',
-            postContent: '',
-            postMessage: '',
-            postId: '',
-            postComments: '',
-            postComment: '',
-            postSubcomments: '',
-            postVotes: '',
-            type: ''
-=======
             comments: [],
             subComments: [],
             showCommentForm: false,
             newComment: ''
->>>>>>> e3fd78d18a69e7ac5134a0fb42722b019a09b441
         }
     },
     mounted: function(){
         this.getSinglePost();
     },
     methods: {
-<<<<<<< HEAD
-        getData: function(){
-            var postData = ps.getSinglePost()
-            //debugger
-            //this.post = postData
-            this.postTitle = postData.posts.title
-            this.postContent = postData.posts.content
-            this.postMessage = postData.message
-            this.postId = postData.posts._id
-            this.postComments = postData.posts.comments 
-            this.postComment = postData.comments 
-            this.postSubcomments = postData.subcomments 
-            this.postVotes = postData.posts.votes
-            this.type = postData.posts.type
-=======
         getSinglePost: function(){
             // let postId = this.$root.$data.currentPostId;
             let postId = '589ec40d15bf7d4632a43696'
@@ -61,51 +34,10 @@ Vue.component('postpage',{
                 // postId: this.$root.$data.currentPostId
             }
             ps.addNewComment(createdComment, this.getSinglePost)
->>>>>>> e3fd78d18a69e7ac5134a0fb42722b019a09b441
         }
     },
     template: `
-    <div id="postpage">
-
-<<<<<<< HEAD
-       <p>{{postTitle}}</p>
-        <p>{{postMessage}}</p>
-        <p>{{postId}}</p>
-        <p>{{postVotes}}</p>
-        <p>{{postContent}}</p>
-        <p>{{postComments}}</p>
-        <p>{{postComment}}</p>
-        <p>{{postSubcomments}}</p>
-        <p>All above this is good.</p>
-
-        <div v-if="type == 'link'">
-            <p>This is the vue for a link.</p> 
-            <p>We need data for the username form the server.</p>
-            <p>Vote: {{postVotes}}</p>
-             <a :href="postContent">{{postContent}}</a>
-            <p>We need data for the date form the server.</p>
-        </div>
-
-        <div v-if="type == 'question'">
-            <p>This is the vue for a question.</p> 
-            <p>We need data for the username form the server.</p>
-            <p>Vote: {{postVotes}}</p>
-            <p>{{postContent}}      </p>
-            <p>We need data for the date form the server.</p>
-        </div>
-
-        <div v-if="type == 'image'">
-            <p>This is the vue for a image.</p> 
-            <p>We need data for the username form the server.</p>
-            <p>Vote: {{postVotes}}</p>
-            <img  width="200" :src="postContent">
-            <p>We need data for the date form the server.</p>
-        </div>
-     
-
-       <hr>
-       </div>
-=======
+    <div v-if="this.$root.$data.postPage"  id="postpage">
        <p>{{post.title}}</p>
        <p>{{post.username}}</p>
         <p>{{post.content}}</p>
@@ -133,7 +65,6 @@ Vue.component('postpage',{
             <textarea id="textarea1" class="materialize-textarea" v-model="newComment"></textarea>
           <button class="waves-effect waves-light btn" type="submit">Submit</button>
           </div>
->>>>>>> e3fd78d18a69e7ac5134a0fb42722b019a09b441
     </div>
         `
  
